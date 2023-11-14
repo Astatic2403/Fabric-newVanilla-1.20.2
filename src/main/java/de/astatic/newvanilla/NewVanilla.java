8cfd78f4a1616faf17bb8f3f49fa2e6aaa98bbd1
Astatic2403 <aleksej.ranke@proton.me>
2023-11-14 12:41:04 +0100

    Update

--- src/main/java/de/astatic/newvanilla/NewVanilla.java
+++ src/main/java/de/astatic/newvanilla/NewVanilla.java
@@ -1,5 +1,6 @@
 package de.astatic.newvanilla;
 
+import de.astatic.newvanilla.item.ModItems;
 import net.fabricmc.api.ModInitializer;
 
 import org.slf4j.Logger;
@@ -11,6 +12,6 @@ public class NewVanilla implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
-		System.out.println("");
+		ModItems.registerModItems();
 	}
 }
---
+++ src/main/java/de/astatic/newvanilla/item/ModItems.java
@@ -0,0 +1,29 @@
+package de.astatic.newvanilla.item;
+
+import de.astatic.newvanilla.NewVanilla;
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroupEntries;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
+
+public class ModItems {
+
+    public static final Item RAW_ASTANITE = registerItem("raw_astanite.json", new Item(new FabricItemSettings()));
+
+    private static void addItemsToIngredientItemGroup(FabricItemGroupEntries entires) {
+        entires.add(RAW_ASTANITE);
+    }
+    private static Item registerItem(String name, Item item) {
+        return Registry.register(Registries.ITEM, new Identifier(NewVanilla.MOD_ID, name), item);
+    }
+    public static void registerModItems() {
+        NewVanilla.LOGGER.debug("Registering Mod Items for " + NewVanilla.MOD_ID);
+
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(ModItems::addItemsToIngredientItemGroup);
+    }
+
+}
